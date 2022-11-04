f203a2c831e6ac9579bb9ed62a573743640ddbe3
xarvioto <xarvioto1@gmail.com>
2022-11-04 12:31:30 +0200

    some more educational refactoring done

--- Homework 3/task1.py
+++ Homework 3/task1.py
@@ -3,45 +3,38 @@
 # символу отримайте за допомогою input() або скористайтеся константою. Наприклад (слово - "Python" а номер символу 3)
 # - "The 3 symbol in 'Python' is 't' ".
 
+def get_num_suffix(num: int) -> str:
+    if num % 10 not in {1, 2, 3} or num % 100 in {11, 12, 13}:
+        return 'th'
+    else:
+        return ('st', 'nd', 'rd')[(num - 1) % 10]
+
 
 max_attempts_allowed = 5
+attempt_counter = 0
 
-# It may be seen as unlogical here to use 'for' instead of 'while', as well as there are too many ways out of loop,
-# one more than necessary
-# Ideally my 'for' shall never reach the end of its last iteration ('max_attempts_allowed + # 1' - 1)
-# But i used 'for' on purpose. ReasoningL 'while' is way less efficient in Python (consumes a way more
-# resources and time while doing the same job) at least i've read such a thing: as 'for' logic in Python is written
-# directly using C, but 'while' in Python is written using Python
-for attempt_count in range(1, max_attempts_allowed + 1):
+while attempt_counter <= max_attempts_allowed:
+    word = input('Веедіть слово/набір символів, в якому будемо шукати символ:').strip(' ')
+
     try:
-
-        word = input('Веедіть слово/набір символів, в якому будемо шукати символ:')
         assert len(word) > 0
-        symp_prompt = f'Введіть порядковий номер символу, який треба витягти зі слова (ціле число від 1 до {len(word)}):'
-        symb_num = int(input(symp_prompt))
-        print('-'* 40)
+        symb_num = int(input(f'Введіть порядковий номер символу, який треба витягти зі слова '
+                             f'(ціле число від 1 до {len(word)}):'))
 
-        num_suffix = 'th' if symb_num % 10 not in {1, 2, 3} or symb_num % 100 in {11, 12, 13} else ('st', 'nd', 'rd')[(symb_num - 1) % 10]
-        print(f"The {symb_num}{num_suffix} symbol in the word '{word}' is '{word[symb_num - 1]}'")
+        print(f'The {symb_num}{get_num_suffix(symb_num)} symbol in the word \"{word}\" is ----> \"{word[symb_num - 1]}\" <----')
         break
 
     except AssertionError:
         print('Помилка вводу: введіть не порожнє слово/набір символів')
-
     except ValueError:
-        print('Помилка типу даних: введіть слово/набір символів, а також ціле число')
-
+        print('Помилка типу даних: введіть число')
     except IndexError:
         print('Помилка у номері символу: номер символу, має бути не більшим за довжину слова/введеного набору символів')
 
-    if attempt_count >= max_attempts_allowed:
-        print('Спроби закінчились. Запустіть код із самого початку')
-        break
-    elif attempt_count == max_attempts_allowed - 1:
-        attempt_count += 1
-        print('-' * 40)
-        print(f'Це Ваша остання спроба, {attempt_count} з {max_attempts_allowed} дозволених')
-    else:
-        attempt_count += 1
-        print('-' * 40)
-        print(f'Спроба {attempt_count} з {max_attempts_allowed} дозволених')
+    attempt_counter += 1
+    print('-'* 40)
+    print(f'{"Це Ваша остання спроба," if attempt_counter == max_attempts_allowed - 1 else "Спроба"} '
+          f'{attempt_counter} з {max_attempts_allowed} дозволених')
+
+else:
+    print('Спроби закінчились. Запустіть код із самого початку')
--- Homework 4/task1.py
+++ Homework 4/task1.py
@@ -3,14 +3,14 @@
 
 vowels_set = {'a', 'e', 'u', 'i', 'o'}
 
-input_string = ''
-
-while not input_string:
+while True:
     input_string = input('Please enter some string to check:')
     if not input_string:
         print('Error: a string should be not empty. Please, try entering something again')
+    else:
+        break
 
-list_of_words = input('Please enter some string to check:').lower().split(' ')
+list_of_words = input_string.lower().split(' ')
 
 vowel_counter = 0
 result_word_counter = 0
@@ -29,4 +29,4 @@ for word in list_of_words:
 print('-' * 40)
 print(f'In your string "{input_string}"\n'
       f'there are ----> {result_word_counter} <---- words\n'
-      f'containing two chars in a row from the set {vowels_set}')
+      f'containing two chars in a row from {vowels_set}')
