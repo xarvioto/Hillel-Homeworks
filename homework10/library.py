d07efa5d6f3ddbed8d7562db0f1f990f7c2ffa34
xarvioto <xarvioto1@gmail.com>
2022-12-11 16:46:34 +0200

    Initial homework10 commit

--- homework10/library.py
+++ homework10/library.py
@@ -8,12 +8,12 @@ def make_positive_power_of_two_real_nums_product(factor_num_one: int | float,
                                                  factor_num_two: int | float,
                                                  /,
                                                  *,
-                                                 power_num: Annotated[int | float, Ge(0)]) -> int | float:
+                                                 power_num: Annotated[int, Ge(0)]) -> int | float:
     int_n_float_set = {int, float}
 
     if not type(factor_num_one) in int_n_float_set \
             or not type(factor_num_two) in int_n_float_set \
-            or not type(power_num) in int_n_float_set:
+            or not type(power_num) == int:
         raise TypeError
 
     if power_num < 0:
--- homework10/test_library.py
+++ homework10/test_library.py
@@ -15,7 +15,6 @@ def test_result_type_for_make_positive_power_of_two_real_nums_product():
     assert type(make_positive_power_of_two_real_nums_product(2, 4, power_num=5)) == int, 'not expected result type'
     assert type(make_positive_power_of_two_real_nums_product(1.5, 2, power_num=3)) == float, 'not expected result type'
     assert type(make_positive_power_of_two_real_nums_product(1, 2.5, power_num=3)) == float, 'not expected result type'
-    assert type(make_positive_power_of_two_real_nums_product(1, 2, power_num=3.5)) == float, 'not expected result type'
 
 
 def test_args_kwargs_typeerror_for_make_positive_power_of_two_real_nums_product():
@@ -35,6 +34,9 @@ def test_num_types_typeerror_for_make_positive_power_of_two_real_nums_product():
 
     with pytest.raises(TypeError):
         make_positive_power_of_two_real_nums_product(2, 3, power_num=[4])
+
+    with pytest.raises(TypeError):
+        make_positive_power_of_two_real_nums_product(2, 4, power_num=4.5)
 
 
 def test_negative_power_valueerror_for_make_positive_power_of_two_real_nums_product():
